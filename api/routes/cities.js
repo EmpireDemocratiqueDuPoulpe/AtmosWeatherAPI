cd96480c91b3deb6bcc1d928a6925901d1abad09
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-03 13:25:28 +0200

    Minor Fixes

--- api/routes/cities.js
+++ api/routes/cities.js
@@ -12,11 +12,11 @@ export default (router) => {
 		const { uid, name } = request.body;
 
 		if(await CityModel.checkIfExist(uid, name)) {
-			return response.status(400).json({ code: 400, message: "The city is already added" }).end();
+			return response.status(400).json({ code: 400, error: "The city is already added" }).end();
 		}
 
 		CityModel.add(uid, name)
-			.then(() => response.status(200).json({ code: 200, message: "City added" }).end())
+			.then(() => response.status(200).json({ code: 200 }).end())
 			.catch(err => response.json({code: 500, error: err.message}).status(500).end());
 	});
 
@@ -45,7 +45,7 @@ export default (router) => {
 				if (status.ok === 1) {
 					response.status(202).json({ code: 202 }).end();
 				} else {
-					response.status(404).json({ code: 404, message: `This user (${uid}) has no city named "${name}"` }).end();
+					response.status(404).json({ code: 404, error: `This user (${uid}) has no city named "${name}"` }).end();
 				}
 			})
 			.catch(err => response.status(500).json({code: 500, error: err.message}).end());
